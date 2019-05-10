eff0a64dac2ed57a3235d423570efbc8c28873cc
Tiffany J Brown <tiffany.brownjc@gmail.com>
2019-05-10 15:29:22 -0400

    created header render w/month and arrows

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,13 +2,46 @@ import React from "react";
 import dateFns from "date-fns";
 
 class Calendar extends React.Component {
-  render() {
+    state = {
+        currentMonth: new Date(),
+        selectedDate: new Date()
+    };
+    renderHeader() {
+        const dateFormat = "MMMM YYYY";
+
+        return (
+            <div className="header row flex-middle">
+                <div className="col col-start">
+                    <div className="icon" onClick={this.prevMonth}>arrow_left</div>
+                </div>
+                <div className="col col-center">
+                    <span>
+                        {dateFns.format(this.state.currentMonth, dateFormat)}
+                    </span>
+                </div>
+                <div className="icon" onClick={this.nextMonth}>arrow_right</div>
+            </div>
+        );
+    }
+    
+    renderDays() {}
+    
+    renderCells() {}
+    
+    onDateClick = day => {}
+    
+    nextMonth = () => {}
+    
+    prevMonth = () => {}
+
+render() {
     return (
-      <div>
-        <div>Header</div>
-        <div>Days</div>
-        <div>Cells</div>
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
       </div>
+
     );
   }
 }
